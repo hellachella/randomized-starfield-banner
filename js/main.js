788d17dcf3c1bf7121dcae487529a613f86c87ff
Michelle <vand0432@algonquinlive.com>
2016-04-04 18:11:49 -0400

    made the stars a random width

--- js/main.js
+++ js/main.js
@@ -3,12 +3,15 @@ var $addStars;
 var $width = $banner.outerWidth();
 var $height = $banner.outerHeight();
 
+
+
 for (i = 0; i < 75; i++) {
   $addStars = $('<div>');
   $addStars.addClass('star');
   $addStars.css('left', Math.random() * 1000);
   $addStars.css('top', Math.random() * 1000);
   $addStars.css('opacity', Math.random() * 10);
+   $addStars.css('width', Math.random() * 1000);
 
 
   $banner.append($addStars);
