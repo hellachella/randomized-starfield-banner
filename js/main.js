be4ab414578576b11c32b00fc153f2e6bf3794e5
Michelle <vand0432@algonquinlive.com>
2016-04-04 17:25:15 -0400

    trying to randomize scale

--- js/main.js
+++ js/main.js
@@ -8,7 +8,8 @@ for (i = 0; i < 75; i++) {
   $addStars.addClass('star');
   $addStars.css('left', Math.random() * 1000);
   $addStars.css('top', Math.random() * 1000);
-  $addStars.css('opacity', Math.random() * 1000);
+  $addStars.css('opacity', Math.random() * 10);
+
 
   $banner.append($addStars);
 };
