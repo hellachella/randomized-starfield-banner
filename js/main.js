c97ca46ec5ef12bc1d3b514814690cec8f8d681b
Michelle <vand0432@algonquinlive.com>
2016-04-05 19:43:32 -0400

    In Class update

--- js/main.js
+++ js/main.js
@@ -3,25 +3,20 @@ var $addStars;
 var $width = $banner.outerWidth();
 var $height = $banner.outerHeight();
 
-
-
 for (i = 0; i < 75; i++) {
   $rotateNum = Math.random() * 1000;
-  $scaleNum = Math.random() * 10000;
-  $animateNum = Math.random() * 50;
+  $scaleNum = Math.random() * 2;
+  $animateNum = Math.random() * 10;
   $addStars = $('<div>');
   $addStars.addClass('star');
   $addStars.css({
-    left: Math.random() * 1000,
-    top: Math.random() * 1000,
+    left: Math.random() * $width,
+    top: Math.random() * $height,
     opacity: Math.random() * 10,
     width: Math.random() * 100,
-    transform: 'rotate(' + $rotateNum + 'deg)',
-    transform: 'scale(' + $scaleNum + '%)',
+    transform: 'rotate(' + $rotateNum + 'deg) scale(' + $scaleNum + ')',
     animation: 'growstars ' + $animateNum + 's alternate infinite'
   });
 
- $banner.append($addStars);
+  $banner.append($addStars);
 };
-
-
