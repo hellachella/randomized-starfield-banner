9f98fce3c0f9ebb74c3157aab5155e8bb6ada63d
Michelle <vand0432@algonquinlive.com>
2016-04-04 20:13:09 -0400

    Rotated stars

--- js/main.js
+++ js/main.js
@@ -6,13 +6,15 @@ var $height = $banner.outerHeight();
 
 
 for (i = 0; i < 75; i++) {
+  $rotateNum = Math.random() * 1000;
   $addStars = $('<div>');
   $addStars.addClass('star');
   $addStars.css({
-    'left': Math.random() * 1000,
-    'top': Math.random() * 1000,
-    'opacity': Math.random() * 10,
-    'width': Math.random() * 1000
+    left: Math.random() * 1000,
+    top: Math.random() * 1000,
+    opacity: Math.random() * 10,
+    width: Math.random() * 100,
+    transform: 'rotate(' + $rotateNum + 'deg)'
   });
 
  $banner.append($addStars);
